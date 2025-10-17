04d641a4e3c05bcc406849b9a0134dfac6d525a4
andrelsrn <andrel.srn@hotmail.com>
2025-10-17 10:45:22 -0300

    Move Main.java para pasta application e renomeio

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
+package application;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Program {
     public static void main(String[] args) {
 
     }
